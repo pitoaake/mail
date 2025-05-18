426df94d381b2fb51b83b13ec13a38bb79f5af11
pitoaake <tyrellsense@gmail.com>
2025-05-18 11:56:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -163,7 +163,8 @@ install_docker_compose() {
             echo "下载失败，尝试下一个镜像源..."
             rm -f "$INSTALL_PATH" 2>/dev/null
             sleep 2
-        done
+        fi
+    done
     
     # 检查是否下载成功
     if [ ! -f "$INSTALL_PATH" ]; then
